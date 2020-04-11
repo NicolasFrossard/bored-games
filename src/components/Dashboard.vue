86b95441e13f20f956d331de1e54665b53921bd6
Nico <frossard.nicolas@gmail.com>
2020-04-12 00:22:12 +0200

    adding element ui

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,6 +3,35 @@
     <h1>{{ msg }}</h1>
     <canvas id="myCanvas" width="680" height="520"></canvas>
     <button @click="sendMsg">Please do not click here</button>
+
+    <el-row>
+      <el-button plain>Plain</el-button>
+      <el-button type="primary" plain>Primary</el-button>
+      <el-button type="success" plain>Success</el-button>
+      <el-button type="info" plain>Info</el-button>
+      <el-button type="warning" plain>Warning</el-button>
+      <el-button type="danger" plain>Danger</el-button>
+    </el-row>
+
+    <el-table
+            :data="tableData"
+            height="250"
+            style="width: 100%">
+      <el-table-column
+              prop="date"
+              label="Date"
+              width="180">
+      </el-table-column>
+      <el-table-column
+              prop="name"
+              label="Name"
+              width="180">
+      </el-table-column>
+      <el-table-column
+              prop="address"
+              label="Address">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -14,7 +43,36 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Bored Games'
+      msg: 'Welcome to Bored Games',
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-08',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-06',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-07',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
     }
   },
   methods: {
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueSocketIO from 'vue-socket.io'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/reset.css'
 
 Vue.config.productionTip = false
 
@@ -15,6 +18,8 @@ Vue.use(new VueSocketIO({
     mutationPrefix: 'SOCKET_'
   },
 }))
+
+Vue.use(Element)
 
 /* eslint-disable no-new */
 new Vue({
