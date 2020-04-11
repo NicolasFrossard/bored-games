74f8082c22afcf88bb505d6b3759d598d4dc5b50
Nico <frossard.nicolas@gmail.com>
2020-04-11 17:45:49 +0200

    experimenting with canvas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import * as socketio from "socket.io";
 import * as path from "path";
+const gameState: any = {
+    players: {}
+};
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -14,16 +17,53 @@ app.get("/", (req: any, res: any) => {
     res.sendFile(path.resolve("./src/client/index.html"));
 });
 
-// whenever a user connects on port 3000 via
-// a websocket, log that a user has connected
-io.on("connection", function(socket: any) {
-    console.log("a user connected");
-    // whenever we receive a 'message' we log it out
+io.on('connection', (socket: any) => {
+    console.log('a user connected:', socket.id);
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected: ', socket.id);
+        delete gameState.players[socket.id]
+    });
+
     socket.on("message", function(message: any) {
         console.log(message);
         socket.emit("message", "I got this from you: " + message);
     });
+
+    socket.on('newPlayer', () => {
+        console.log('we have a new player: ', socket.id);
+        gameState.players[socket.id] = {
+            x: Math.floor(Math.random()*600),
+            y: Math.floor(Math.random()*480),
+            width: 25,
+            height: 25
+        }
+    });
+
+    socket.on('playerMovement', (playerMovement: any) => {
+        const player = gameState.players[socket.id];
+        const canvasWidth = 680;
+        const canvasHeight = 520;
+
+        if (playerMovement.left && player.x > 0) {
+            player.x -= 4
+        }
+        if (playerMovement.right && player.x < canvasWidth - player.width) {
+            player.x += 4
+        }
+
+        if (playerMovement.up && player.y > 0) {
+            player.y -= 4
+        }
+        if (playerMovement.down && player.y < canvasHeight - player.height) {
+            player.y += 4
+        }
+    })
 });
+
+setInterval(() => {
+    io.sockets.emit('state', gameState);
+}, 1000 / 60);
 
 const server = http.listen(3000, function() {
     console.log("listening on *:3000");
