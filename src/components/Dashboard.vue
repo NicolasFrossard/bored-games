fa218d0241042bc1f8044f012c10183407430f7e
Nico <frossard.nicolas@gmail.com>
2020-04-12 00:42:26 +0200

    adding GameLog.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,78 +1,30 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <canvas id="myCanvas" width="680" height="520"></canvas>
-    <button @click="sendMsg">Please do not click here</button>
-
-    <el-row>
-      <el-button plain>Plain</el-button>
-      <el-button type="primary" plain>Primary</el-button>
-      <el-button type="success" plain>Success</el-button>
-      <el-button type="info" plain>Info</el-button>
-      <el-button type="warning" plain>Warning</el-button>
-      <el-button type="danger" plain>Danger</el-button>
+    <el-row gutter="2">
+      <el-col :span="14">
+        <div class="grid-content bg-purple-dark">a</div>
+      </el-col>
+      <el-col :span="10">
+        <div class="grid-content bg-purple-dark">
+          <game-log></game-log>
+        </div>
+      </el-col>
     </el-row>
-
-    <el-table
-            :data="tableData"
-            height="250"
-            style="width: 100%">
-      <el-table-column
-              prop="date"
-              label="Date"
-              width="180">
-      </el-table-column>
-      <el-table-column
-              prop="name"
-              label="Name"
-              width="180">
-      </el-table-column>
-      <el-table-column
-              prop="address"
-              label="Address">
-      </el-table-column>
-    </el-table>
   </div>
 </template>
 
 <script>
+import GameLog from './GameLog'
+
 export default {
-  name: 'HelloWorld',
+  components: {GameLog},
+  name: 'Dashboard',
   mounted() {
     this.initSocket();
   },
   data () {
     return {
-      msg: 'Welcome to Bored Games',
-      tableData: [{
-        date: '2016-05-03',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-02',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-04',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-01',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-08',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-06',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-07',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }]
+
     }
   },
   methods: {
